72ce88f90b0a2c09225a92d176475e410d7180fd
Ayesha <aliayeshanyc@gmail.com>
2019-01-29 12:47:55 -0500

    data.js

---
+++ models/data.js
@@ -0,0 +1,66 @@
+function loadCSV(filename) {
+  var users_file = fs.readFileSync(filename, "utf8");
+  //console.log(users_file);
+  var rows = users_file.split('\n');
+  var user_data = [];
+  for(var i = 0; i < rows.length; i++) {
+      var user_d = rows[i].trim().split(",");
+      var user = {};
+      if (filename == "data/users.csv") {
+        user["name"] = user_d[0];
+        user["pswd"] = user_d[1];
+        user["total_games"] = parseInt(user_d[2]);
+        user["wins"] = parseInt(user_d[3]);
+        user["losses"] = parseInt(user_d[4]);
+        user["rock"] = parseFloat(user_d[5]);
+        user["paper"] = parseFloat(user_d[6]);
+        user["scissors"] = parseFloat(user_d[7]);
+        user_data.push(user);
+      } else if (filename == "data/villains.csv") {
+        user["name"] = user_d[0];
+        user["total_games"] = parseInt(user_d[1]);
+        user["wins"] = parseInt(user_d[2]);
+        user["losses"] = parseInt(user_d[3]);
+        user["rock"] = parseFloat(user_d[4]);
+        user["paper"] = parseFloat(user_d[5]);
+        user["scissors"] = parseFloat(user_d[6]);
+        user_data.push(user);
+      }
+  }
+  return user_data;
+}
+
+//uploads the csv containing all the gameplay data (used to update villains.csv and users.csv)
+function upLoadCSV(user_data, file_name) {
+  var out="";
+  user_data.sort(function(a,b) {
+    var bPercent = 0;
+    if (b.total_games == 0) {
+      bPercent = 0;
+    } else {
+      bPercent =Math.round((b.wins/b.total_games)*100);
+    }
+    var aPercent = 0;
+    if (a.total_games == 0) {
+      aPercent = 0;
+    } else {
+      aPercent =Math.round((a.wins/a.total_games)*100);
+    }
+    return (bPercent-aPercent);
+  });
+  for (var i = 0; i < user_data.length; i++) {
+    arr=Object.keys(user_data[i]);
+    for (var k=0;k<arr.length;k++){
+      if(k == arr.length-1) {
+        out+=user_data[i][arr[k]];
+      } else {
+        out+=user_data[i][arr[k]]+",";
+      }
+    }
+    if (i!=user_data.length-1){
+        out+="\n";
+    }
+
+  }
+  fs.writeFileSync(file_name, out, "utf8")
+}
