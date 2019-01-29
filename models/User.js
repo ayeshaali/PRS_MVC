79f5de4be0f3ed3092c5ca15e57c06d0e0b1a359
ayeshaali <aliayeshanyc@gmail.com>
2019-01-29 18:48:40 -0500

    users.js model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var dataJS = require(__dirname +'/data');
+var villainJS = require(__dirname +'/Villain');
 
 
 exports.getUser = function(user_id) {
@@ -14,6 +15,34 @@ exports.getUser = function(user_id) {
   return user;
 }
 
+exports.createUser = function(user_id, user_password) {
+  var all_users = dataJS.loadCSV("data/users.csv");
+  var user_data = {}
+  user_data["name"] = user_id;
+  user_data["pswd"] = user_password;
+  user_data["games"] =0;
+  user_data["total_games"] =0;
+  user_data["wins"] =0;
+  user_data["losses"] =0;
+  user_data["rock"] =0;
+  user_data["paper"] =0;
+  user_data["scissors"] = 0;
+  all_users.push(user_data);
+  dataJS.uploadCSV(all_users, "data/users.csv");
+} 
+
+exports.deleteUser = function(user_id) {
+  var all_users = dataJS.loadCSV("data/users.csv");
+  var index;
+  for(var i=1; i<all_users.length; i++){
+    if(all_users[i].name==user_id.trim()){
+      index = i;
+    }
+  }
+  all_users.splice(i,1);
+  dataJS.uploadCSV(all_users, "data/users.csv");
+}
+
 exports.updateUser = function(user_id, new_info) {
   console.log("Users.getUser");
   var user={
@@ -23,16 +52,18 @@ exports.updateUser = function(user_id, new_info) {
   return user;
 }
 
-exports.createUser = function(user_id, user_password) {
-  
-  user_data["games"] =0;
-  user_data["total_games"] =0;
-  user_data["wins"] =0;
-  user_data["losses"] =0;
-  user_data["rock"] =0;
-  user_data["paper"] =0;
-  user_data["scissors"] = 0;
-} 
+exports.handleThrow = function(userWeapon, villain, villainPrevious, userPrevious){
+    villainWeapon=villainJS.villainStrategies(villain,villainPrevious,userPrevious,userWeapon);
+    switch(userWeapon){
+        case villainWeapon:
+          return("drew");
+        case villainJS.winAgainst(villainWeapon):
+            return("won");
+        case villainJS.loseAgainst(villainWeapon):
+            return("lost");
+    }
+}
+
 var createBlankUser= function(){
   var user={
     name:"test",
