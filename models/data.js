95442fa3917b25f6f6c54f6383ef8900ff3b7714
ayeshaali <aliayeshanyc@gmail.com>
2019-02-10 19:50:05 -0500

    starting with spreadsheets

--- models/data.js
+++ models/data.js
@@ -1,3 +1,19 @@
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('../client_secret.json');
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet('1DVgMG20OgfLR0leaJvzOiHDxp19EoyGKHTJxUCnxoX0');
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(1, function (err, rows) {
+    console.log(rows);
+  });
+});
+
+
+
+
+//CSV stuff---------------------------------
 var fs = require("fs");
 
 exports.loadCSV =function(filename) {
